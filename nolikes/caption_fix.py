0096171a531d8f5b05c1304594533af56a08ff1b
sam hains <sam@samhains.com>
2017-04-16 18:20:24 +0200

    fix caption fix bug

--- nolikes/caption_fix.py
+++ nolikes/caption_fix.py
@@ -1,20 +1,26 @@
 from pattern.en import verbs, lemma, lexeme, parsetree, Sentence
+sentence = "a man holding a cell phone in his hand"
+
 
 def fix_caption(str):
     s = parsetree(str, lemmata=True)
     string = ''
     for sentence in s:
         for i, chunk in enumerate(sentence.chunks):
+            print(chunk)
             if chunk.type == 'VP' and len(chunk) == 2:
                 print('@!!')
                 verb = chunk[1].string
                 string += lexeme(verb)[1]+' '
             else:
                 for j, w in enumerate(chunk.words):
-                    if i == 0 and j == 0:
+                    if i == 0 and j == 0 and (w.string == 'a' or w.string == 'A'):
+                        print w.string
                         pass
                     else:
                         string = string + w.string+' '
 
     return string[:1].upper() + string[1:-1]
 
+
+print(fix_caption(sentence))

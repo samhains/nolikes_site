046a6a016a1395d6bdc54a57a81ec9cdbf682cfd
Ubuntu <ubuntu@ip-172-31-46-224.us-west-2.compute.internal>
2017-04-16 16:09:36 +0000

    fix

--- nolikes/populate.py
+++ nolikes/populate.py
@@ -1,4 +1,5 @@
 from nolikes import db, Image
+from sqlalchemy import exc
 from caption_fix import fix_caption
 import json
 
@@ -6,13 +7,21 @@ IMG_DIR = '~/data/images/'
 
 image_json = json.load(open('vis.json'))
 
+Image.query.delete()
+db.session.commit()
+
+i = 0
 for data in image_json:
     uuid = data['file_name'].split('/')[-1].split('.')[0]
     fname = IMG_DIR+data['file_name'].split('/')[-1]
     caption = fix_caption(data['caption'])
     image = Image(uuid, fname, caption)
+    i = i+1
+    print('saving image id:', i)
     db.session.add(image)
-    db.session.commit()
+    try:
+      db.session.commit()
+    except exc.SQLAlchemyError, e:
+      print(e)
 
 images = Image.query.all()
-print(images)
